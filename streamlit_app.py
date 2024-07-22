5159e47d577d11e7b2281051645ff7437a943ce1
Theodora Wrobel von Zuben <126608702+TWvonZuben@users.noreply.github.com>
2024-07-22 11:17:52 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-import requirements
+#import requirements
 import sklearn
 from sklearn.ensemble import RandomForestRegressor
 from io import BytesIO
