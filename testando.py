237c1faba36eb04af8107dd1379ddecdfee28c42
Theodora Wrobel von Zuben <126608702+TWvonZuben@users.noreply.github.com>
2024-07-22 15:00:51 -0300

    Update testando.py

--- testando.py
+++ testando.py
@@ -11,8 +11,8 @@ import pandas as pd
 import numpy as np
 import pickle
 import requests
-#import sklearn
-#from sklearn.ensemble import RandomForestRegressor
+import sklearn
+from sklearn.ensemble import RandomForestRegressor
 from io import BytesIO
 
 
